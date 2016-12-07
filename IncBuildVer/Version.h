e73c1e7bbbaf54bb07edfde56dde975fb2d7db20
Song Baoming <songbaoming@yeah.net>
2016-12-07 09:32:27 +0800

    Change the icon and correct the misspelling

--- IncBuildVer/Version.h
+++ IncBuildVer/Version.h
@@ -8,7 +8,7 @@
 #pragma once
 
 // °æ±¾ºÅ
-#define MARJOR_VER_NUM					1 // Ö÷°æ±¾ºÅ
+#define MAJOR_VER_NUM					1 // Ö÷°æ±¾ºÅ
 #define MINOR_VER_NUM					0 // ´Î°æ±¾ºÅ
 #define REVISION_VER_NUM				0 // ÐÞÕý°æ±¾ºÅ
 #define BUILD_VER_NUM					4 // ±àÒë°æ±¾ºÅ
@@ -17,4 +17,4 @@
 // °æ±¾ºÅ×Ö·û´®
 #define TOSTR0(arg) #arg
 #define TOSTR(arg) TOSTR0(arg)
-#define VERSION_STR TOSTR(MARJOR_VER_NUM) "." TOSTR(MINOR_VER_NUM) "." TOSTR(REVISION_VER_NUM) "." TOSTR(BUILD_VER_NUM)
+#define VERSION_STR TOSTR(MAJOR_VER_NUM) "." TOSTR(MINOR_VER_NUM) "." TOSTR(REVISION_VER_NUM) "." TOSTR(BUILD_VER_NUM)
